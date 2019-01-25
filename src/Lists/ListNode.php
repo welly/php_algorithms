7f9a36b3e239e13d20b24225a7f4fa39aa1b61fd
Alastair Moore <alastair.moore@protonmail.com>
2019-01-25 14:33:13 +0000

    Defined ListNode with constructor and member variables

--- src/Lists/ListNode.php
+++ src/Lists/ListNode.php
@@ -3,5 +3,23 @@
 namespace welly\PHPAlgorithms\Lists;
 
 class ListNode {
-	
+
+  /**
+   * @var null Contains ListNode data.
+   */
+	public $data = NULL;
+
+  /**
+   * @var null "Pointer" to next ListNode item.
+   */
+	public $next = NULL;
+
+  /**
+   * ListNode constructor.
+   *
+   * @param null $data
+   */
+	public function __construct($data = NULL) {
+		$this->data = $data;
+	}
 }
