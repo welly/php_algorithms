286dccb9409fe1d04497204ea04e32c0de28fb76
Alastair Moore <alastair.moore@protonmail.com>
2019-02-01 11:57:49 +0000

    Added test for Repository pattern, phunit config

---
+++ tests/RepositoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use welly\PHPAlgorithms\Repository\Comment;
+
+class RepositoryTest extends \PHPUnit\Framework\TestCase {
+
+	function testACommentHasAllItsComposingParts() {
+		$postId = 1;
+		$commentAuthor = "Joe";
+		$commentAuthorEmail = "joe@example.com";
+    $commentSubject = "Joe Has an Opinion about the Repository Pattern";
+    $commentBody = "I think it is a good idea to use the Repository Pattern to persist and retrieve objects.";
+    $comment = new Comment($postId, $commentAuthor, $commentAuthorEmail, $commentSubject, $commentBody);
+
+    $this->assertEquals($postId, $comment->getPostId());
+    $this->assertEquals($commentAuthor, $comment->getAuthor());
+    $this->assertEquals($commentAuthorEmail, $comment->getAuthorEmail());
+    $this->assertEquals($commentSubject, $comment->getSubject());
+    $this->assertEquals($commentBody, $comment->getBody());
+
+	}
+}
