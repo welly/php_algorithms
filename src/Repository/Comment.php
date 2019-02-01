d830f5da53e43483792a9460109dc883f3e2f99b
Alastair Moore <alastair.moore@protonmail.com>
2019-02-01 11:58:02 +0000

    Comment class to be used with Repository pattern

---
+++ src/Repository/Comment.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace welly\PHPAlgorithms\Repository;
+
+/**
+ * Class Comment
+ *
+ * @package welly\PHPAlgorithms\Factory
+ */
+class Comment {
+
+  private $postId;
+  private $author;
+  private $authorEmail;
+  private $subject;
+  private $body;
+
+  /**
+   * Comment constructor.
+   *
+   * @param $postId
+   * @param $author
+   * @param $authorEmail
+   * @param $subject
+   * @param $body
+   */
+  public function __construct($postId, $author, $authorEmail, $subject, $body) {
+    $this->postId = $postId;
+    $this->author = $author;
+    $this->authorEmail = $authorEmail;
+    $this->subject = $subject;
+    $this->body = $body;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getPostId() {
+    return $this->postId;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getAuthor() {
+    return $this->author;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getAuthorEmail() {
+    return $this->authorEmail;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getSubject() {
+    return $this->subject;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getBody() {
+    return $this->body;
+  }
+
+}
