3992dd3eb72ec779976474746cc6190068f187c6
Alastair Moore <alastair.moore@protonmail.com>
2019-02-01 12:36:26 +0000

    Added CommentFactory test

---
+++ tests/CommentFactoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use welly\PHPAlgorithms\Factory\CommentFactory;
+
+class CommentFactoryTest extends \PHPUnit\Framework\TestCase {
+
+  function testACommentIsComplete() {
+    $postId = 1;
+    $commentAuthor = "Joe";
+    $commentAuthorEmail = "joe@example.com";
+    $commentSubject = "Joe Has an Opinion about the Repository Pattern";
+    $commentBody = "I think it is a good idea to use the Repository Pattern to persist and retrieve objects.";
+    $commentData = [$postId, $commentAuthor, $commentAuthorEmail, $commentSubject, $commentBody];
+
+    $comment = (new CommentFactory())->make($commentData);
+
+    $this->assertEquals($postId, $comment->getPostId());
+    $this->assertEquals($commentAuthor, $comment->getAuthor());
+    $this->assertEquals($commentAuthorEmail, $comment->getAuthorEmail());
+    $this->assertEquals($commentSubject, $comment->getSubject());
+    $this->assertEquals($commentBody, $comment->getBody());
+  }
+}
