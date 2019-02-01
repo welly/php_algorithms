4ef06a2f9b4383ebe87e7a3d76e9fb2f64514256
Alastair Moore <alastair.moore@protonmail.com>
2019-02-01 12:35:41 +0000

    Added new test for multiple data items

--- tests/InMemoryPersistenceTest.php
+++ tests/InMemoryPersistenceTest.php
@@ -1,9 +1,12 @@
 <?php
 
-use welly\PHPAlgorithms\Repository\InMemoryPersistence;
+use welly\PHPAlgorithms\Utils\InMemoryPersistence;
 
 class InMemoryPersistenceTest extends PHPUnit\Framework\TestCase {
 
+  /**
+   * Testing InMemoryPersistence class to see if it can store and retrieve data.
+   */
   function testItCanPersistAndRetrieveASingleDataArray() {
     $data = array('data');
 
@@ -12,4 +15,21 @@ class InMemoryPersistenceTest extends PHPUnit\Framework\TestCase {
 
     $this->assertEquals($data, $persistence->retrieve(0));
   }
+
+  /**
+   * Testing InMemoryPersistence class to see if it can store/retrieve multiple
+   * data values.
+   */
+  function testItCanPerisistSeveralElementsAndRetrieveAnyOfThem() {
+    $data1 = array('data1');
+    $data2 = array('data2');
+
+    $persistence = new InMemoryPersistence();
+    $persistence->persist($data1);
+    $persistence->persist($data2);
+
+    $this->assertEquals($data1, $persistence->retrieve(0));
+    $this->assertEquals($data2, $persistence->retrieve(1));
+  }
+
 }
